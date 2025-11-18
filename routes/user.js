e9705efcb7993881d075c90785498dc1fb8b62ac
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-11-18 23:59:55 +0530

    creatingMiddleware

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,33 @@ const router = express.Router();
  
 
 const {login, signup} = require("../controllers/Auth");
+const {auth, isStudent, isAdmin} =require("../middlewars/auth");
 
 router.post("/login", login);
 router.post("/signup", signup);
+
+
+//testing protected route for single middkeware 
+router.get("/test", auth, isStudent, (req,res) => {
+    res.json({
+        success:true,
+        message:"welcom to protected router for Test"
+    });
+});
+
+//protected route
+router.get("/student", auth, isStudent, (req,res) => {
+    res.json({
+        success:true,
+        message:"welcom to protected router for Student"
+    });
+});
+
+router.get("/admin", auth, isStudent, (req,res) => {
+    res.json({
+        success:true,
+        message:"welcom to protected router for Admin"
+    });
+});
 
 module.exports = router;
